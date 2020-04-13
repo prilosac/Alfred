8139a3c9aa04d575fec7c778780ef3e020fce9b9
Lucas Teixeira <prilosac@gmail.com>
2020-04-13 13:39:24 -0500

    added to stats

--- p3/stats.py
+++ p3/stats.py
@@ -3,6 +3,11 @@ class Stats:
         self.total_frames = 0
         self.skipped_frames = 0
         self.thinking_time = 0
+        self.stocks_taken = 0
+        self.stocks_lost = 0
+        self.damage_recieved = 0
+        self.damage_done = 0
+        self.prevState = None
 
     def __str__(self):
         if not self.total_frames:
@@ -20,3 +25,24 @@ class Stats:
 
     def add_thinking_time(self, thinking_time):
         self.thinking_time += thinking_time
+
+    def add_metrics(self, state):
+        if self.prevState is not None:
+            self.add_stocks_taken(state)
+            self.add_stocks_lost(state)
+            self.add_damage_done(state)
+            self.add_damage_recieved(state)
+        
+        self.prevState = state
+    
+    def add_stocks_taken(self, state):
+        self.stocks_taken += self.prevState.players[0].__dict__['stocks'] - state.players[0].__dict__['stocks']
+
+    def add_stocks_lost(self, state):
+        self.stocks_lost += self.prevState.players[3].__dict__['stocks'] - state.players[3].__dict__['stocks']
+
+    def add_damage_done(self, state):
+        self.damage_done += state.players[0].__dict__['percent'] - self.prevState.players[0].__dict__['percent']
+    
+    def add_damage_recieved(self, state):
+        self.damage_recieved += state.players[3].__dict__['percent'] - self.prevState.players[3].__dict__['percent']
